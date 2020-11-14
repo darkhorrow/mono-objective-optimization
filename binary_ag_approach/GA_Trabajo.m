158954b7b6887a737b65ad8875bfe17a164d3ead
U-DESKTOP-H4LOGIE\Juan Ramirez <juanseraar@hotmail.com>
2020-11-14 17:41:28 +0000

    Se reescribió el binario

--- binary_ag_approach/GA_Trabajo.m
+++ binary_ag_approach/GA_Trabajo.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%ALGORITMO GENETICO: Diseï¿½o de un voladizo de secciï¿½n rectangular y 
-%                    longitud dada para conseguir peso mï¿½nimo
+%ALGORITMO GENETICO: Diseño de un voladizo de sección rectangular y 
+%                    longitud dada para conseguir peso mínimo
 %SOLUCION: El valor de las variables del problema son:
 %    0.5    0.0928
-%El valor de la funciï¿½n objetivo es: 4.641589
-%El valor de la restricciï¿½n (no lineal): 0.994583
+%El valor de la función objetivo es: 4.641589
+%El valor de la restricción (no lineal): 0.994583
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 inicializa();
 
@@ -20,15 +20,9 @@ nvars=4;
 lb=[0.0625,0.0625,10,10]; 
 %Limite superior de los valores de las variables 
 ub=[0.0625*99,0.0625*99,200,240]; 
-<<<<<<< HEAD
 %Número de decimales de precisión
 dprec = 5;
 %Número de bits con que se codifica cada variable
-=======
-%Nï¿½mero de decimales de precisiï¿½n
-dprec = 4;
-%Nï¿½mero de bits con que se codifica cada variable
->>>>>>> 57cd869e64fcd542d2da482f2b9161fd60bf2963
 nbits = zeros(1,nvars);
 tot_bits = 0;
 for i=1:1:nvars
@@ -45,38 +39,22 @@ G=150;
 N=500;
 %Numero de INDIVIDUOS ELITISTAS
 E=1;  %si E=0 se sobreentiende que no se aplica elitismo
-<<<<<<< HEAD
 %Operador de selección:    0:proporcional    1:torneo probabilístico    2:torneo    
 tipo_seleccion = 2;
 %Probabilidad de selección del mejor para el torneo probabilístico (1)
 psel = 0.75;
 %Número de padres que intervienen en el torneo (2): 2, 3 o 5
 nuintor = 5;
-=======
-%Operador de selecciï¿½n:    0:proporcional    1:torneo probabilï¿½stico    2:torneo    
-tipo_seleccion=2;
-%Probabilidad de selecciï¿½n del mejor para el torneo probabilï¿½stico (1)
-psel=0.75;
-%Nï¿½mero de padres que intervienen en el torneo (2): 2, 3 o 5
-nuintor = 3;
->>>>>>> 57cd869e64fcd542d2da482f2b9161fd60bf2963
 %Operador de cruce:   0:cruce un punto   1:cruce dos puntos   2:cruce uniforme
 tipo_cruce = 2;
 %Probabilidad de cruce (prob. se realice cruce)
-<<<<<<< HEAD
 pcru = 0.8;
 %Operador de mutación:   0:un bit   1:dos bits    2:uniforme
 tipo_mutacion = 0;
 %Cada cuántas generaciones se dibuja el valor del mejor
-=======
-pcru=0.8;
-%Operador de mutaciï¿½n:   0:un bit   1:dos bits    2:uniforme
-tipo_mutacion=2;
-%Cada cuï¿½ntas generaciones se dibuja el valor del mejor
->>>>>>> 57cd869e64fcd542d2da482f2b9161fd60bf2963
 ngendibujo = 10; 
-%nï¿½mero de ensayos diferentes que se quieren realizar
-ensayos=10;    
+%número de ensayos diferentes que se quieren realizar
+ensayos=1;    
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                      % GRAN BUCLE QUE VA GENERANDO LOS ENSAYOS 
@@ -84,13 +62,13 @@ ensayos=10;
 for jj=1:ensayos
   
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-   ti=cputime;              %recoge tiempo cuando empieza la optimizaciï¿½n   
+   ti=cputime;              %recoge tiempo cuando empieza la optimización   
    [mejor,fmejor]=GA(jj,ensayos);
  
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                         % IMPRIMIR-GRAFICAR RESULTADOS 
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-   tf=cputime-ti;            %recoge tiempo cuando termina la optimizaciï¿½n
+   tf=cputime-ti;            %recoge tiempo cuando termina la optimización
    muestratiempo(jj,tf);     %mostrar tiempos
 
    if ensayos == 1
@@ -99,7 +77,7 @@ for jj=1:ensayos
        format long
        disp('El valor de las variables del problema son:')
        disp(mejor)
-       disp('El valor de la funciï¿½n objetivo es:')
+       disp('El valor de la función objetivo es:')
        disp(fmejor)
        fprintf('\n');
    end
